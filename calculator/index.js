b2c16b32a58fb71d61bb7735b944e86ba2186fd8
nidhisharma63 <nidhisharma639593@gmail.com>
2022-04-26 21:03:58 +0530

    project7 add sum mult divide fun added

--- calculator/index.js
+++ calculator/index.js
@@ -2,12 +2,14 @@ import {
     addHoverEffect
 } from './js/btns.style.js';
 import {
-    getValue
+    getValue,
+    Action
 } from './js/get-value.js'
 const btns = document.querySelectorAll('.btn');
 const result = document.querySelector(".result");
 const onBtn = document.getElementById("on-btn");
-const clear = document.getElementById("clear")
+const clear = document.getElementById("clear");
+const equal = document.getElementById("equal")
 let on = false;
 onBtn.addEventListener("click", () => {
     on = true;
@@ -21,4 +23,18 @@ clear.addEventListener("click", () => {
     if (on) {
         result.innerHTML = ''
     }
+});
+equal.addEventListener('click',()=>{
+    if(on){
+        Action()
+    }
 })
+
+const add =(arr)=>{
+    let sum=0;
+    for(let i=0;i<arr.length;i++){
+        sum+=parseInt(arr[i])
+    }
+    console.log(sum)
+}
+// add([21,3,4])
--- calculator/js/get-value.js
+++ calculator/js/get-value.js
@@ -1,11 +1,68 @@
-// const btns = document.querySelectorAll('.btn');
+const result = document.querySelector(".result");
+let totalResultInnerHtml;
 export const getValue = (btns, result) => {
     btns.forEach((btn) => {
         btn.addEventListener("click", () => {
-            let totalLenghtResultInnerHtml = String(result.innerHTML);
-            if (totalLenghtResultInnerHtml.length < 21) {
-                result.innerHTML += btn.getAttribute("value")
+
+            totalResultInnerHtml = String(result.innerHTML);
+            if (totalResultInnerHtml.length < 21) {
+                result.innerHTML += btn.getAttribute("value");
             }
+            totalResultInnerHtml = String(result.innerHTML);
         });
     });
+}
+const add = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += parseInt(arr[i]);
+    }
+    result.innerHTML = sum;
+}
+const sub = (arr) => {
+    let sub = 0;
+    for (let i = 0; i < arr.length-1; i++) {
+        sub += (parseInt(arr[i])-parseInt(arr[i+1]))
+    }
+    result.innerHTML = sub;
+}
+const mult = (arr) => {
+    let mult = 1;
+    for (let i = 0; i < arr.length; i++) {
+        mult *= parseInt(arr[i]);
+    }
+    result.innerHTML = mult;
+}
+const divide = (arr) => {
+    let divide=0;
+    for (let i = 0; i < arr.length-1; i++) {
+        divide = (parseInt(arr[i])/parseInt(arr[i+1]));
+    }
+    result.innerHTML = divide;
+}
+export const Action = (equal) => {
+    let splitValue;
+    for (let i = 0; i < totalResultInnerHtml.length; i++) {
+        switch (totalResultInnerHtml[i]) {
+            case '+':
+                splitValue = totalResultInnerHtml.split("+");
+                add(splitValue);
+                break;
+            case '-':
+                splitValue = totalResultInnerHtml.split("-");
+                sub(splitValue);
+                break;
+            case '/':
+                splitValue = totalResultInnerHtml.split("/");
+                divide(splitValue)
+                break;
+            case '%':
+                splitValue = totalResultInnerHtml.split("%")
+                break;
+            case '*':
+                splitValue = totalResultInnerHtml.split("*");
+                mult(splitValue)
+                break;
+        }
+    }
 }
