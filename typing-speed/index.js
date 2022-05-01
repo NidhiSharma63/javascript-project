d612d4fe8dfd8a83124ede9ae79d3ede039051b4
nidhisharma63 <nidhisharma639593@gmail.com>
2022-05-01 10:18:08 +0530

    project9 (speed testing game) completed

--- typing-speed/index.js
+++ typing-speed/index.js
@@ -1,25 +1,34 @@
 const paragraphText = document.querySelector(".paragraph-text");
 const text = [
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, ut. Repellendus molestias animi omnis facilis recusandae provident dignissimos tempore perferendis sunt, quos enim qui, dolorum molestiae? Dolorem nulla cumque molestiae'
-]
+];
 const userTextInput = document.getElementById("user-text");
 const startbBtn = document.getElementById("start-btn");
-const restartbtn = document.getElementById("restart-btn");
+const tryAgain = document.getElementById("restart-btn");
 const timerContainer = document.querySelector(".timer");
-const errorContainer = document.getElementById("error")
-let alredyStart = false;
-let presetTimer = 30
+const errorContainer = document.getElementById("error");
+let start = false;
+let presetTimer = 50
 let timer = presetTimer;
 let index = 0;
 let error = 0;
 timerContainer.innerHTML = timer;
-const appendText = () => {
+// APPENDING TEXT IN PARAGRAPH
+const appendText = async () => {
     const splitText = text[0].split('');
     splitText.forEach((char, ind) => {
         const span = document.createElement("span");
         span.innerText = char;
         paragraphText.appendChild(span);
     });
+};
+
+// LISTEN FOR INPUT EVENT
+const inputEvent = async () => {
+    await appendText();
+    start = true;
+    const spans = paragraphText.querySelectorAll('span');
+    userTextInput.setAttribute("maxlength", `${spans.length}`)
     userTextInput.addEventListener("input", () => {
         const spans = paragraphText.querySelectorAll('span');
         const userTextValue = userTextInput.value.split('');
@@ -29,37 +38,45 @@ const appendText = () => {
             spans[index].classList.add('incorrect');
             error++;
             errorContainer.innerHTML = `error:<span class=error-count>${error}</span>`
-        }
+        };
         index++;
     });
-}
+    timeLeft();
+};
+
+inputEvent();
 // RESTART BTN
 const DisplayRestartBtn = () => {
     startbBtn.style.display = "none";
-    restartbtn.style.display = "flex";
+    tryAgain.style.display = "flex";
     return;
-}
-// RESTART BTN EVENT LISTENER
-restartbtn.addEventListener("click", () => {
-    alredyStart = false;
-    startbBtn.style.display = "flex";
-    restartbtn.style.display = "none";
-    timer = presetTimer;
-});
-// START BTN
-startbBtn.addEventListener('click', () => {
+};
 
-    if (!alredyStart) {
+const timeLeft = () =>{
+    if (start) {
         let x = setInterval(() => {
             timer--;
             if (timer <= -1) {
                 clearInterval(x);
-                timer = presetTimer;
+                timer =0;
                 DisplayRestartBtn();
+                userTextInput.disabled = true,
+                start = false;
             }
             timerContainer.innerHTML = timer;
-        }, 1000);
-    }
-    alredyStart = true
+        }, 1500);
+    };
+}
+// tryAgain BTN EVENT LISTENER
+tryAgain.addEventListener("click", () => {
+    userTextInput.disabled = false,
+    start=true;
+    timeLeft()
+    timer=presetTimer;
+    error=0;
+    timerContainer.innerHTML = timer;
+    userTextInput.value='';
+    errorContainer.innerHTML = `error:<span class=error-count>${error}</span>`;
+    startbBtn.style.display = "flex";
+    tryAgain.style.display = "none";
 });
-appendText();
